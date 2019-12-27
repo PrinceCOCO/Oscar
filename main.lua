720365649bea99eae4a8a7b973e74471c2efa5c6
PrinceCOCO <59289629+PrinceCOCO@users.noreply.github.com>
2019-12-27 17:24:06 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,87 @@
+--Salut je suis oscar 
+--[[ je suis oscar 
+zeub zeub 
+]]--
+age = 20 
+prenom = "oui"
+tmoche = true = 1 false = 0
+a, b, c = 1, 5, "hello"
+a = 10 --global
+local b = 25 --locale
+a, b = 5, 3
+c = a + b
+a, b = "bonjour, ", "Je suis une chaine de caractère"
+
+
+maListe = {
+	5,
+	7,
+	2,
+	6,
+	4
+}
+
+a, b, c = maListe[1], maListe[2], maListe[3]
+
++
+-
+*
+/
+
+maListe =  {"Bernard"}
+a = maListe[1]
+
+if age == 20 then 
+print("J'ai 20 ans")
+elseif age <= 18
+print("je suis majeur")
+else 
+print("J'ai moins de 18 ans")
+end
+
+a = 10
+a == 10 -- Égale à 10?
+a > 5 --  supérieur à 5?
+a < 5 --  inférieur et plus petit à 5?
+a >= 7 -- Supérieur ou égale à 7?
+a <= 7 -- Inférieur ou égale à 7?
+a ~= 5 -- Différent de 5?
+
+not a == b
+
+a > 1 and b < 20 then 
+end 
+
+a > 1 or b < 20 then 
+end
+
+
+a, b, c, d = 10, "fivem", true, false
+
+
+a= 10
+b= "fivem"
+c= true 
+d= false
+
+
+if a > 10 and not d then
+	elseif b == "fivem" and (a == 10 or d) then
+	else
+end
+
+local negatif = true == 1 
+local maVariable = (1 and -1 or 1) * maVariable
+
+
+-1 == "Infinie"
+
+tamere = -1
+
+if tamere == -1 then
+	print("Bah c'est ta mère !")
+else
+	print("bah non frérot c'est pas ta mère !")
+end
+
+
